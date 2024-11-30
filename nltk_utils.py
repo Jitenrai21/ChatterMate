c06c13a0442ae756c2c0494024bdc4ac847bbd8b
Jiten Rai <raijiten96@gmail.com>
2024-11-30 22:47:35 +0545

    initial commit

---
+++ nltk_utils.py
@@ -0,0 +1,15 @@
+import nltk
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
